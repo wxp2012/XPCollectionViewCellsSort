46481e7d56de03a804670b230846e6bce2a6542a
wxp2012 <wxpnews@gmail.com>
2017-02-13 23:35:00 +0800

    创建工程
    Signed-off-by: wxp2012 <wxpnews@gmail.com>

---
+++ XPCollectionViewCellsSort/XPCollectionViewCellsSort/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XPCollectionViewCellsSort
+//
+//  Created by Carlw on 2017/2/13.
+//  Copyright © 2017年 wxp2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XPCollectionViewCellsSort/XPCollectionViewCellsSort/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XPCollectionViewCellsSort
+//
+//  Created by Carlw on 2017/2/13.
+//  Copyright © 2017年 wxp2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
